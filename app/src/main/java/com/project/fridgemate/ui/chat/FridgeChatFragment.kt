9bfbcebfed700a4b6a7336b529034c6e264e4e41
Moshik Baruch <moshikb@elementor.com>
2026-06-19 03:28:19 +0300

    fix chat badge bug

--- app/src/main/java/com/project/fridgemate/ui/chat/FridgeChatFragment.kt
+++ app/src/main/java/com/project/fridgemate/ui/chat/FridgeChatFragment.kt
@@ -8,12 +8,14 @@ import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.project.fridgemate.databinding.FragmentFridgeChatBinding
+import com.project.fridgemate.ui.fridge.FridgeViewModel
 
 class FridgeChatFragment : Fragment() {
 
@@ -22,6 +24,7 @@ class FridgeChatFragment : Fragment() {
 
     private val args: FridgeChatFragmentArgs by navArgs()
     private val viewModel: FridgeChatViewModel by viewModels()
+    private val sharedFridgeViewModel: FridgeViewModel by activityViewModels()
 
     private lateinit var adapter: MessageAdapter
     private lateinit var layoutManager: LinearLayoutManager
@@ -120,6 +123,16 @@ class FridgeChatFragment : Fragment() {
         val last = layoutManager.findLastVisibleItemPosition()
         val total = adapter.itemCount
         return total == 0 || last >= total - 2
+    }
+
+    override fun onStart() {
+        super.onStart()
+        sharedFridgeViewModel.setChatOpen(true)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        sharedFridgeViewModel.setChatOpen(false)
     }
 
     override fun onPause() {
